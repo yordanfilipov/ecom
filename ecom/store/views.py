4acf81aaf48d00dc34db0c8a54721294ac2eab7b
yordanfilipov <jordanfdimitrov@gmail.com>
2024-09-17 15:44:56 +0300

    search products

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
+from django.db.models import Q
 import logging
 
 # logging.basicConfig(level=logging.INFO,
@@ -15,6 +16,38 @@ import logging
 #                     )
 
 # logger = logging.getLogger(__name__)
+
+def search(request):
+    # determine if they filled out the form
+    if request.method == "POST":
+        searched = request.POST['searched']  # name="searched" form search.html page
+        # query the products DB model
+        searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+
+        # test for null
+        if not searched:
+            messages.success(request, "That Product Doesn't Exist.. Please Try Again.")
+            return render(request, "search.html", {})
+        else:
+            return render(request, "search.html", {'searched': searched})
+    else:
+        return render(request, "search.html", {})
+
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "Your Info Has Been Updated!")
+            return redirect('home')
+        return render(request, "update_info.html", {"form": form})
+    else:
+        messages.success(request, "You Must Be Logged In To Access That Page!")
+        return redirect('home')
 
 def update_password(request):
     if request.user.is_authenticated:
@@ -119,8 +152,8 @@ def register_user(request):
             # log in user
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, 'You have registered successfully! Welcome!')
-            return redirect('home')
+            messages.success(request, 'Username Created - Please Fill Out Your User Info Below..')
+            return redirect('update_info')
         else:
             messages.success(request, 'There was a problem during registration. Please try again!')
             return redirect('register')
