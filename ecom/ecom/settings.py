c5519ce88525cac06efcae76c3f9d42e2076a5cb
Yordan Dimitrov <jordanfdimitrov@gmail.com>
2024-10-25 14:40:02 +0300

    clickable images/secret key environment

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -13,7 +13,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2c+12!yv57ikw-9!@=4wiq4rz00zgyd$3-lionibr%2=@f=f0c'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
