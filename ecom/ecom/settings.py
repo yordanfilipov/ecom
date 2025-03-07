899f2e784639c91d59db59731f644966764b5cd9
Yordan Dimitrov <jordanfdimitrov@gmail.com>
2025-03-07 15:39:32 +0200

    commit

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
-CSRF_TRUSTED_ORIGINS = ['https://techecom-yordans-projects-4c7b2c13.vercel.app']
+ALLOWED_HOSTS = [".vercel.app"]
+#CSRF_TRUSTED_ORIGINS = ['https://techecom-yordans-projects-4c7b2c13.vercel.app']
 
 
 # Application definition
@@ -75,21 +75,24 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': os.environ['DB_PASSWORD_YO'],
-        'HOST': 'autorack.proxy.rlwy.net',
-        'PORT': '15205',
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': os.environ['DB_PASSWORD_YO'],
+#         'HOST': 'autorack.proxy.rlwy.net',
+#         'PORT': '15205',
         
-    }
-}
+#     }
+# }
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    "default": dj_database_url.config(env="DATABASE_URL", conn_max_age=600)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
